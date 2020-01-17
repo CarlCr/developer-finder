196e7b538b41e03d599fda92c254f108f6019fd9
Carlos Garcia <gtxcarl9@gmail.com>
2020-01-17 03:04:59 +0100

    Cors added in index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 const app = express()
 
@@ -10,6 +11,7 @@ mongoose.connect('mongodb+srv://<user>:<password>@cluster0-efoe3.mongodb.net/tes
   useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.listen(3333)
