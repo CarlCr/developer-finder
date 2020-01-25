b9269f8a80f926afd1d4b3e5e4f0383704b4b603
carlcr <gtxcarl9@gmail.com>
2020-01-25 01:38:24 +0100

    Aside forms created

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './Global.css'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-        <aside></aside>
+    <div id="App">
+        <aside>
+          <strong>Register</strong>
+        <form>
+          <div className="input_block">
+            <label htmlFor="github_user_name">Github User</label>
+            <input name="github_username" id="github_user_name" required/>
+          </div>
+          <div className="input_block">
+            <label htmlFor="techs">Techs</label>
+            <input name="techs" id="techs" required/>
+          </div>
+          <div className="input-group">
+            <div className="input_block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+
+            <div className="input_block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+          <button type="submit">Save</button>
+        </form>
+        </aside>
         <main></main>
     </div>
   );
