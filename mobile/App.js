c833c6a88803ff9ffafb8a9d29761bea2e73a90e
carlcr <gtxcarl9@gmail.com>
2020-01-26 00:47:11 +0100

    installed react-navigation

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>CarlCr</Text>
     </View>
   );
 }
@@ -15,5 +15,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:'#333'
   },
+  text:{
+    fontSize:30,
+    color:'#fff',
+    fontWeight:'700'
+  }
 });
---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {View} from 'react-native'
+
+function Main(){
+    return(
+
+    )
+}
+export default Main
